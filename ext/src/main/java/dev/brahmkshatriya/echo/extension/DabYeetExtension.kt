375079bf031b628e29167a1a52b994550fb1d358
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-07-19 00:39:05 +0600

    text class

--- ext/src/main/java/dev/brahmkshatriya/echo/extension/DabYeetExtension.kt
+++ ext/src/main/java/dev/brahmkshatriya/echo/extension/DabYeetExtension.kt
@@ -1,10 +1,19 @@
 package dev.brahmkshatriya.echo.extension
 
 import dev.brahmkshatriya.echo.common.clients.ExtensionClient
+import dev.brahmkshatriya.echo.common.clients.SearchFeedClient
 import dev.brahmkshatriya.echo.common.settings.Setting
 import dev.brahmkshatriya.echo.common.settings.Settings
+import dev.brahmkshatriya.echo.common.helpers.PagedData
+import dev.brahmkshatriya.echo.common.models.Feed
+import dev.brahmkshatriya.echo.common.models.QuickSearchItem
+import dev.brahmkshatriya.echo.common.models.Shelf
+import dev.brahmkshatriya.echo.common.models.Tab
 
-class DabYeetExtension : ExtensionClient {
+class DabYeetExtension : ExtensionClient, SearchFeedClient {
+
+    private val baseUrl = "https://dab.yeet.su/api/"
+
     override suspend fun onExtensionSelected() {}
 
     override val settingItems: List<Setting> = emptyList()
@@ -12,5 +21,23 @@ class DabYeetExtension : ExtensionClient {
     private lateinit var setting: Settings
     override fun setSettings(settings: Settings) {
         setting = settings
+    }
+    
+    //==== SearchFeedClient ====//
+
+    override suspend fun quickSearch(query: String): List<QuickSearchItem> {
+        TODO("Implement")
+    }
+
+    override suspend fun deleteQuickSearch(item: QuickSearchItem) {
+        TODO("Implement")
+    }
+
+    override suspend fun searchTabs(query: String): List<Tab> {
+        TODO("Implement")
+    }
+
+    override fun searchFeed(query: String, tab: Tab?): Feed {
+        TODO("Implement")
     }
 }
